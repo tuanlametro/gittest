893545eafdf79da1ad66dd32c8d57c030c6386e3
Joshua Waugh <joshua@waugh.zone>
2018-11-05 11:37:24 +0200

    Zumo project beginning
    Initial files for the project

---
+++ Zumo/ZumoBot.cydsn/main.c
@@ -0,0 +1,524 @@
+/**
+* @mainpage ZumoBot Project
+* @brief    You can make your own ZumoBot with various sensors.
+* @details  <br><br>
+    <p>
+    <B>General</B><br>
+    You will use Pololu Zumo Shields for your robot project with CY8CKIT-059(PSoC 5LP) from Cypress semiconductor.This 
+    library has basic methods of various sensors and communications so that you can make what you want with them. <br> 
+    <br><br>
+    </p>
+    
+    <p>
+    <B>Sensors</B><br>
+    &nbsp;Included: <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;LSM303D: Accelerometer & Magnetometer<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;L3GD20H: Gyroscope<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Reflectance sensor<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Motors
+    &nbsp;Wii nunchuck<br>
+    &nbsp;TSOP-2236: IR Receiver<br>
+    &nbsp;HC-SR04: Ultrasonic sensor<br>
+    &nbsp;APDS-9301: Ambient light sensor<br>
+    &nbsp;IR LED <br><br><br>
+    </p>
+    
+    <p>
+    <B>Communication</B><br>
+    I2C, UART, Serial<br>
+    </p>
+*/
+#include <project.h>
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "Motor.h"
+#include "Ultra.h"
+#include "Nunchuk.h"
+#include "Reflectance.h"
+#include "Gyro.h"
+#include "Accel_magnet.h"
+#include "LSM303D.h"
+#include "IR.h"
+#include "Beep.h"
+#include "mqtt_sender.h"
+#include <time.h>
+#include <sys/time.h>
+#include "serial1.h"
+#include <unistd.h>
+#define coeff 5/4095; /*Vref divided by the number of steps between 0 and max*/
+#define realv 5/3; /*Used to find real voltage, as the ADC reading is 3/5ths of the multimeter reading*/
+/**
+ * @file    main.c
+ * @brief   
+ * @details  ** Enable global interrupt since Zumo library uses interrupts. **<br>&nbsp;&nbsp;&nbsp;CyGlobalIntEnable;<br>
+*/
+
+#if 0
+// Hello World!
+void zmain(void)
+{
+    printf("\nHello, World!\n");
+
+    while(true)
+    {
+        vTaskDelay(100); // sleep (in an infinite loop)
+    }
+ }   
+#endif
+
+#if 0
+// Name and age
+void zmain(void)
+{
+    char name[32];
+    int age;
+    
+    
+    printf("\n\n");
+    
+    printf("Enter your name: ");
+    //fflush(stdout);
+    scanf("%s", name);
+    printf("Enter your age: ");
+    //fflush(stdout);
+    scanf("%d", &age);
+    
+    printf("You are [%s], age = %d\n", name, age);
+
+    while(true)
+    {
+        BatteryLed_Write(!SW1_Read());
+        vTaskDelay(100);
+    }
+ }   
+#endif
+
+
+#if 0
+//battery level//
+// BatteryLed_Write(1); // Switch led on 
+// BatteryLed_Write(0); // Switch led off 
+// SW1_Read() returns zero when button is pressed
+// SW1_Read() returns one when button is not pressed
+void ledloop();
+void batcheck();
+int16 adcresult = 0;
+float volts = 0.0;
+bool warning = false;
+
+void zmain(void)
+{   
+    ADC_Battery_Start();
+    ADC_Battery_StartConvert();
+    while(1)
+    {
+        if (warning == false)
+        {
+            batcheck();
+        }
+        else if (warning == true)
+        {
+            ledloop();
+        }
+    }
+}
+
+void batcheck()
+{
+    while(warning == false)
+    {
+        ADC_Battery_IsEndConversion(ADC_Battery_WAIT_FOR_RESULT); // wait for ADC converted value
+        adcresult = ADC_Battery_GetResult16(); // get the ADC value (0 - 4095)
+        float converted = adcresult*coeff;
+        volts = converted * realv;
+        printf("%d %f\r\n",adcresult, volts);
+        if (volts <= 4.0)
+        {
+            warning = true;
+        }
+        vTaskDelay(500);
+    }
+}
+
+void ledloop()
+{
+    uint8 i = 0;
+    
+    while(warning == true)
+    {
+        Beep(500, i);
+        i++;
+        BatteryLed_Write(1);
+        vTaskDelay(500);
+        Beep(500, i);
+        i++;
+        BatteryLed_Write(0);
+        vTaskDelay(500);
+        if(SW1_Read() == 0)
+        {
+            warning = false;
+        }
+    }
+}
+#endif
+
+#if 0
+// button
+void zmain(void)
+{
+    uint8 button = SW1_Read();
+    void S()
+    {
+        for(int i = 0; i <= 2; i++)
+        {
+            printf("S");
+            BatteryLed_Write(1);
+            vTaskDelay(100);
+            BatteryLed_Write(0);
+            vTaskDelay(100);  
+        }
+    }
+    
+    void O()
+    {
+        for(int i = 0; i <= 2; i++)
+        {
+            printf("O");
+            BatteryLed_Write(1);
+            vTaskDelay(300);
+            BatteryLed_Write(0);
+            vTaskDelay(100);  
+        }
+    }
+    while(button == 0) 
+    {
+        /* printf("Press button within 5 seconds!\n");
+        int i = 50;
+        while(i > 0) {
+            if(SW1_Read() == 0) {
+                break;
+            }
+            vTaskDelay(100);
+            --i;
+        }
+        if(i > 0) {
+            printf("Good work\n");
+            while(SW1_Read() == 0) vTaskDelay(10); // wait until button is released
+        }
+        else {
+            printf("You didn't press the button\n");
+        }*/
+        S();
+        O();
+        S();
+    }
+}
+#endif
+
+#if 0
+// button
+void zmain(void)
+{
+    printf("\nBoot\n");
+
+    //BatteryLed_Write(1); // Switch led on 
+    BatteryLed_Write(0); // Switch led off 
+    
+    //uint8 button;
+    //button = SW1_Read(); // read SW1 on pSoC board
+    // SW1_Read() returns zero when button is pressed
+    // SW1_Read() returns one when button is not pressed
+    
+    bool led = false;
+    
+    for(;;)
+    {
+        // toggle led state when button is pressed
+        if(SW1_Read() == 0) {
+            led = !led;
+            BatteryLed_Write(led);
+            if(led) printf("Led is ON\n");
+            else printf("Led is OFF\n");
+            Beep(1000, 150);
+            while(SW1_Read() == 0) vTaskDelay(10); // wait while button is being pressed
+        }        
+    }
+ }   
+#endif
+
+
+#if 1
+void motor_tank_turn(uint8 l_speed, uint8 r_speed, uint32 delay);
+void zmain(void)
+{
+    Ultra_Start(); 
+    motor_start();              // enable motor controller
+    motor_forward(0,0);
+    vTaskDelay(3000);
+    
+                            // Ultra Sonic Start function
+    while(1) 
+    {
+        int d = Ultra_GetDistance();
+        // Print the detected distance (centimeters)
+        printf("distance = %d\r\n", d);
+        if( d <= 10 )
+        {
+            Beep(500, 10);
+            motor_backward(0,0);
+            motor_turn(100, 10, 2000);
+        }
+        else 
+        {
+            motor_forward(0,0);
+            
+        
+        vTaskDelay(200);
+    }
+    //motor_turn(100, 100, 2000);
+}  
+    
+void motor_tank_turn(uint8 l_speed, uint8 r_speed, uint32 delay)
+{
+    MotorDirLeft_Write(0);      // set LeftMotor backward mode
+    MotorDirRight_Write(0);
+    PWM_WriteCompare1(l_speed); 
+    PWM_WriteCompare2(r_speed); 
+    vTaskDelay(delay);
+}
+#endif
+
+#if 0
+//IR receiverm - how to wait for IR remote commands
+void zmain(void)
+{
+    IR_Start();
+    
+    printf("\n\nIR test\n");
+    
+    IR_flush(); // clear IR receive buffer
+    printf("Buffer cleared\n");
+    
+    bool led = false;
+    // Toggle led when IR signal is received
+    for(;;)
+    {
+        IR_wait();  // wait for IR command
+        led = !led;
+        BatteryLed_Write(led);
+        if(led) printf("Led is ON\n");
+        else printf("Led is OFF\n");
+    }    
+ }   
+#endif
+
+
+
+#if 0
+//IR receiver - read raw data
+void zmain(void)
+{
+    IR_Start();
+    
+    uint32_t IR_val; 
+    
+    printf("\n\nIR test\n");
+    
+    IR_flush(); // clear IR receive buffer
+    printf("Buffer cleared\n");
+    
+    // print received IR pulses and their lengths
+    for(;;)
+    {
+        if(IR_get(&IR_val, portMAX_DELAY)) {
+            int l = IR_val & IR_SIGNAL_MASK; // get pulse length
+            int b = 0;
+            if((IR_val & IR_SIGNAL_HIGH) != 0) b = 1; // get pulse state (0/1)
+            printf("%d %d\r\n",b, l);
+        }
+    }    
+ }   
+#endif
+
+
+#if 0
+//reflectance
+void zmain(void)
+{
+    struct sensors_ ref;
+    struct sensors_ dig;
+
+    reflectance_start();
+    reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center sensor threshold to 11000 and others to 9000
+    
+
+    for(;;)
+    {
+        // read raw sensor values
+        reflectance_read(&ref);
+        // print out each period of reflectance sensors
+        printf("%5d %5d %5d %5d %5d %5d\r\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       
+        
+        // read digital values that are based on threshold. 0 = white, 1 = black
+        // when blackness value is over threshold the sensors reads 1, otherwise 0
+        reflectance_digital(&dig); 
+        //print out 0 or 1 according to results of reflectance period
+        printf("%5d %5d %5d %5d %5d %5d \r\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);        
+        
+        vTaskDelay(200);
+    }
+}   
+#endif
+
+
+#if 0
+//motor
+void zmain(void)
+{
+    motor_start();              // enable motor controller
+    motor_forward(0,0);         // set speed to zero to stop motors
+
+    vTaskDelay(3000);
+    
+    motor_forward(100,2000);     // moving forward
+    motor_turn(200,50,2000);     // turn
+    motor_turn(50,200,2000);     // turn
+    motor_backward(100,2000);    // moving backward
+     
+    motor_forward(0,0);         // stop motors
+
+    motor_stop();               // disable motor controller
+    
+    for(;;)
+    {
+
+    }
+}
+#endif
+
+#if 0
+/* Example of how to use te Accelerometer!!!*/
+void zmain(void)
+{
+    struct accData_ data;
+    
+    printf("Accelerometer test...\n");
+
+    if(!LSM303D_Start()){
+        printf("LSM303D failed to initialize!!! Program is Ending!!!\n");
+        vTaskSuspend(NULL);
+    }
+    else {
+        printf("Device Ok...\n");
+    }
+    
+    for(;;)
+    {
+        LSM303D_Read_Acc(&data);
+        printf("%8d %8d %8d\n",data.accX, data.accY, data.accZ);
+        vTaskDelay(50);
+    }
+ }   
+#endif    
+
+#if 0
+// MQTT test
+void zmain(void)
+{
+    int ctr = 0;
+
+    printf("\nBoot\n");
+    send_mqtt("Zumo01/debug", "Boot");
+
+    //BatteryLed_Write(1); // Switch led on 
+    BatteryLed_Write(0); // Switch led off 
+
+    for(;;)
+    {
+        printf("Ctr: %d, Button: %d\n", ctr, SW1_Read());
+        print_mqtt("Zumo01/debug", "Ctr: %d, Button: %d", ctr, SW1_Read());
+
+        vTaskDelay(1000);
+        ctr++;
+    }
+ }   
+#endif
+
+
+#if 0
+void zmain(void)
+{    
+    struct accData_ data;
+    struct sensors_ ref;
+    struct sensors_ dig;
+    
+    printf("MQTT and sensor test...\n");
+
+    if(!LSM303D_Start()){
+        printf("LSM303D failed to initialize!!! Program is Ending!!!\n");
+        vTaskSuspend(NULL);
+    }
+    else {
+        printf("Accelerometer Ok...\n");
+    }
+    
+    int ctr = 0;
+    reflectance_start();
+    for(;;)
+    {
+        LSM303D_Read_Acc(&data);
+        // send data when we detect a hit and at 10 second intervals
+        if(data.accX > 1500 || ++ctr > 1000) {
+            printf("Acc: %8d %8d %8d\n",data.accX, data.accY, data.accZ);
+            print_mqtt("Zumo01/acc", "%d,%d,%d", data.accX, data.accY, data.accZ);
+            reflectance_read(&ref);
+            printf("Ref: %8d %8d %8d %8d %8d %8d\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       
+            print_mqtt("Zumo01/ref", "%d,%d,%d,%d,%d,%d", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);
+            reflectance_digital(&dig);
+            printf("Dig: %8d %8d %8d %8d %8d %8d\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);
+            print_mqtt("Zumo01/dig", "%d,%d,%d,%d,%d,%d", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);
+            ctr = 0;
+        }
+        vTaskDelay(10);
+    }
+ }   
+
+#endif
+
+#if 0
+void zmain(void)
+{    
+    RTC_Start(); // start real time clock
+    
+    RTC_TIME_DATE now;
+
+    // set current time
+    now.Hour = 12;
+    now.Min = 34;
+    now.Sec = 56;
+    now.DayOfMonth = 25;
+    now.Month = 9;
+    now.Year = 2018;
+    RTC_WriteTime(&now); // write the time to real time clock
+
+    for(;;)
+    {
+        if(SW1_Read() == 0) {
+            // read the current time
+            RTC_DisableInt(); /* Disable Interrupt of RTC Component */
+            now = *RTC_ReadTime(); /* copy the current time to a local variable */
+            RTC_EnableInt(); /* Enable Interrupt of RTC Component */
+
+            // print the current time
+            printf("%2d:%02d.%02d\n", now.Hour, now.Min, now.Sec);
+            
+            // wait until button is released
+            while(SW1_Read() == 0) vTaskDelay(50);
+        }
+        vTaskDelay(50);
+    }
+ }   
+#endif
+
+/* [] END OF FILE */
